62ee3375eea32f7df78e0429d9563d2718ffc77f
zhanghui <zhanghui15@baijiahulian.com>
2021-02-19 16:00:12 +0800

    feat: 目录结构重构

---
+++ src/main/java/book/q1/Solution.java
@@ -0,0 +1,10 @@
+package book.q1;
+
+/**
+ * 模版类
+ *
+ * @author iterators
+ * @date 2021/2/19 下午3:58
+ */
+public class Solution {
+}
--- src/main/java/video/q3/Solution.java
+++ src/main/java/video/q3/Solution.java
@@ -1,4 +1,4 @@
-package q3;
+package video.q3;
 
 import java.util.HashSet;
 import java.util.Set;
