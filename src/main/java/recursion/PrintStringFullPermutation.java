279eb979e65c67cdcbd2ea8680e7546bd4fc2ba8
iterators <iterators@ushareit.com>
2021-06-24 23:28:15 +0800

    feat: 全排列的解法1

--- src/main/java/recursion/PrintStringFullPermutation.java
+++ src/main/java/recursion/PrintStringFullPermutation.java
@@ -1,9 +1,6 @@
 package recursion;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 字符串的子序列
@@ -15,19 +12,25 @@ public class PrintStringFullPermutation {
 
     public static void main(String[] args) {
         String str = "aac";
-        String result = getFullPermutation(str);
-        System.out.println(result);
-        String permutationNoRepeat = getFullPermutationNoRepeat(str);
-        System.out.println("permutationNoRepeat = " + permutationNoRepeat);
+        String subset = getSubset(str);
+        System.out.println(subset);
+        String subsetNoRepeat = getSubsetNoRepeat(str);
+        System.out.println("subsetNoRepeat = " + subsetNoRepeat);
+
+        String str2 = "abc";
+        System.out.println("getFullPermutation(str2) = " + getFullPermutation(str2));
     }
 
-    private static String getFullPermutation(String origin) {
+    /**
+     * 打印全部子集
+     */
+    private static String getSubset(String origin) {
         List<String> result = new ArrayList<>();
         process(result, origin, 0, "");
         return result.toString();
     }
 
-    private static String getFullPermutationNoRepeat(String origin) {
+    private static String getSubsetNoRepeat(String origin) {
         Set<String> set = new HashSet<>();
         process(set, origin, 0, "");
         return set.toString();
@@ -62,4 +65,36 @@ public class PrintStringFullPermutation {
         // 在当前子序列上添加当前位置值
         process(result, origin, index + 1, path += currentIndexContent);
     }
+
+    /**
+     * 获取字符串的全排列
+     */
+    public static String getFullPermutation(String str) {
+        char[] chars = str.toCharArray();
+        List<List<String>> result = new LinkedList<>();
+        LinkedList<String> path = new LinkedList<>();
+        process(result, chars, path);
+        return result.toString();
+    }
+
+    private static void process(List<List<String>> result, char[] chars, LinkedList<String> path) {
+        //  满足条件，加入到候选集合中
+        if (path.size() == chars.length) {
+            result.add(new LinkedList<>(path));
+            return;
+        }
+
+        for (char c : chars) {
+            // 过滤
+            if (path.contains(c + "")) {
+                continue;
+            }
+            path.add(c + "");
+            process(result, chars, path);
+            path.removeLast();
+        }
+
+    }
+
+
 }
