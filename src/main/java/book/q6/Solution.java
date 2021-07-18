09d4e5c0780345de254064ae01ca0e9c4f47cb18
iterators <iterators@ushareit.com>
2021-07-18 22:32:13 +0800

    feat: 暴力递归求解最小路径和

--- src/main/java/book/q6/Solution.java
+++ src/main/java/book/q6/Solution.java
@@ -9,24 +9,36 @@ package book.q6;
 public class Solution {
 
     public static void main(String[] args) {
-        int[][] m = new int[4][4];
-
+        int[][] m = {{1, 3, 5, 9}, {8, 1, 3, 4}, {5, 0, 6, 1}, {8, 8, 4, 0}};
         int solution = minWays(m);
+        System.out.println("solution = " + solution);
     }
 
     private static int minWays(int[][] m) {
         int x = m.length;
         int y = m[0].length;
-        return f(x-1, y-1);
+        return f(m,x-1, y-1);
     }
 
     /**
      * f函数返回矩阵中m[i][j]位置的最小路径和
      */
-    private static int f(int i, int j) {
+    private static int f(int[][] m, int x, int y) {
         // base case
+        if (x == 0 && y == 0) {
+            return m[x][y];
+        }
 
-        //
-        return 0;
+        // 最左边数据, 只能是可以往下移动的位置过来的
+        if (x == 0) {
+            return m[0][y] + f(m, 0, y - 1);
+        }
+
+        // 最顶层的数据, 只能是可以往右移动的位置过来的
+        if (y == 0) {
+            return m[x][0] + f(m, x - 1, 0);
+        }
+        // 最小路径和是当前位置的m[x][y] + Math.min(到达当前位置左边位置的最小路径和, 达到当前位置的上边位置的最小路径和
+        return Math.min(f(m, x-1, y), f(m, x, y-1)) + m[x][y];
     }
 }
