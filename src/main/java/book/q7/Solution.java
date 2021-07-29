31cf82090be4373701919972e5dc5df0c21194ff
iterators <iterators@ushareit.com>
2021-07-29 22:00:01 +0800

    feat:  动态规划求解换钱的最小货币数

--- src/main/java/book/q7/Solution.java
+++ src/main/java/book/q7/Solution.java
@@ -13,6 +13,9 @@ public class Solution {
         int aim = 1;
         int ways = f(arr, 0, aim);
         System.out.println("ways = " + ways);
+
+        int dpWays = dp(arr, aim);
+        System.out.println("dpWays = " + dpWays);
     }
 
     private static int f(int[] arr, int index, int rest) {
@@ -31,5 +34,39 @@ public class Solution {
             }
         }
         return ways == Integer.MAX_VALUE ? -1 : ways;
+    }
+
+    private static int dp(int[] arr, int aim) {
+        // dp[i][j] 表示只使用任意多张货币arr[0..i]组成aim=j使用的最少货币数
+        int[][] dp = new int[arr.length][aim + 1];
+
+        // Base Case1: 初始化dp数组的第一列dp[i][0]
+        for (int i = 0; i < arr.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        // Base Case2: dp[0][j] 表示只使用arr[0]组成货币j的最小数
+        for (int i = 1; i < aim + 1; i++) {
+            if (i / arr[0] * arr[0] == i) {
+                dp[0][i] = i / arr[0];
+            } else {
+                dp[0][i] = Integer.MAX_VALUE;
+            }
+        }
+        // 迭代求解 dp[i][j] = Math.min(dp[i-1][j-arr[i] * k] + k), arr[i] * k <= j,k为整数且k>=0
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = 0; k * arr[i] <= j; k++) {
+                    // 子问题有效，更新dp数组
+                    int subProblem = dp[i - 1][j - k * arr[i]];
+                    if (subProblem != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(subProblem + k, dp[i][j]);
+                    }
+                }
+            }
+        }
+
+        return dp[arr.length - 1][aim] == Integer.MAX_VALUE ? -1 : dp[arr.length - 1][aim];
     }
 }
