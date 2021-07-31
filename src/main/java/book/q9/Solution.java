afe759589dfd181c249b9b03e6e73c6300b4ab1a
iterators <iterators@ushareit.com>
2021-07-31 14:53:18 +0800

    feat:  动态规划求解换钱的方法数

--- src/main/java/book/q9/Solution.java
+++ src/main/java/book/q9/Solution.java
@@ -11,9 +11,11 @@ public class Solution {
     public static void main(String[] args) {
         int[] arr = {5, 10, 25, 1};
         // int[] arr = {3, 5};
-        int aim = 2;
+        int aim = 15;
         int result = f(arr, 0, aim);
         System.out.println("result = " + result);
+        int dpResult = dp(arr, aim);
+        System.out.println("dpResult = " + dpResult);
     }
 
     private static int f(int[] arr, int index, int aim) {
@@ -29,5 +31,32 @@ public class Solution {
         }
 
         return result;
+    }
+
+
+    /**
+     * 动态规划：dp[i]表示使用数组元素arr[0...j]组成目标金额aim=i的方法数
+     */
+    private static int dp(int[] arr, int aim) {
+        int[] dp = new int[aim + 1];
+
+        // Base case: dp[0]
+        dp[0] = 1;
+
+        // 使用数组元素arr[0]构成aim的方法数
+        // dp的所有元素的默认值为0，意味着组成dp[j]的方法数为0
+        for (int i = 1; i <= aim; i++) {
+            dp[i] = i % arr[0] == 0 ? 1 : 0;
+        }
+
+        // dp[i][j] = (1) + (2)
+        // (1): dp[i-1][j] 使用数组元素arr[0...i-1]组成货币j的方法数
+        // (2): dp[i][j-arr[i]] 使用数组元素arr[0...i]组成货币j-arr[i]的方法数
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                dp[j] += j - arr[i] >= 0 ? dp[j - arr[i]] : 0;
+            }
+        }
+        return dp[aim];
     }
 }
