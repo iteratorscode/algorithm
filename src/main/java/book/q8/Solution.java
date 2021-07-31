8181be6f83d495ae818b0d6d8d69dc22a44d237d
iterators <iterators@ushareit.com>
2021-07-31 13:36:44 +0800

    feat:  动态规划求解换钱的方法数（每个货币可以只能使用一次）

--- src/main/java/book/q8/Solution.java
+++ src/main/java/book/q8/Solution.java
@@ -14,8 +14,13 @@ public class Solution {
 
         int result = f(arr, 0, aim);
         System.out.println("result = " + result);
+        int dpResult = dp(arr, aim);
+        System.out.println("dpResult = " + dpResult);
     }
 
+    /**
+     * 暴力递归求解：f(arr, i, aim) 表示使用arr[i...N]种货币，组成aim的最少货币数
+     */
     private static int f(int[] arr, int i, int aim) {
         if (i == arr.length || aim == 0) {
             return aim == 0 ? 0 : -1;
@@ -35,6 +40,36 @@ public class Solution {
         }
 
         return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+
+    /**
+     * 动态规划求解：dp[i]组成货币i的最少货币数
+     */
+    private static int dp(int[] arr, int aim) {
+        if (arr == null || aim == 0 || arr.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[aim + 1];
+        // Base case: 只使用arr[0]初始化数组dp
+        // Integer.MAX_VALUE 暗示无法组成货币，而非是字面意义的最大值
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = i == arr[0] ? 1 : Integer.MAX_VALUE;
+        }
+
+        // dp数组迭代: dp[i] = Math.min(dp[i], dp[i-arr[j]] + 1)
+        for (int i = 1; i < arr.length; i++) {
+            // 使用arr[0...i]构成aim的最小方法数
+            for (int j = 1; j < dp.length; j++) {
+                int subProblem = Integer.MAX_VALUE;
+                if (j - arr[i] >= 0 && dp[j - arr[i]] != Integer.MAX_VALUE) {
+                    subProblem = dp[j - arr[i]] + 1;
+                }
+                dp[j] = Math.min(subProblem,  dp[j]);
+            }
+        }
+        return dp[aim] == Integer.MAX_VALUE ? -1 : dp[aim];
     }
 
 }
