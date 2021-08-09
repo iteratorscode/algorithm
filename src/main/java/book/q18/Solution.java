afa25747b6d4e4a0d8a42f9e13c39fe3802f4656
iterators <iterators@ushareit.com>
2021-08-09 23:07:19 +0800

    feat: 数字字符串转换为字母组合的种数 init

---
+++ src/main/java/book/q18/Solution.java
@@ -0,0 +1,15 @@
+package book.q18;
+
+/**
+ * 数字字符串转换为字母组合的种数
+ *
+ * @implNote https://www.bilibili.com/video/BV11f4y1677m?p=1
+ * @author iterators
+ * @since 2021/08/09
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
