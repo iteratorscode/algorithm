fad63e29e698d531b574a3292d78c50266325ab1
iterators <iterators@ushareit.com>
2021-08-22 22:46:04 +0800

    feat: 二分搜索查找左右边界

--- src/main/java/labuladong/q3/Solution.java
+++ src/main/java/labuladong/q3/Solution.java
@@ -37,4 +37,51 @@ public class Solution {
 
         return -1;
     }
+
+    private static int leftBound(int[] items, int target) {
+        int low = 0;
+        int high = items.length -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) >>> 1;
+            if (items[mid] == target) {
+                // 向左收缩右边界
+                high = mid - 1;
+            } else if (items[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        // 循环的结束条件为 low == high + 1
+        if (low == items.length || items[low] != target) {
+            return -1;
+        }
+
+        return low;
+    }
+
+    private static int rightBound(int[] items, int target) {
+        int low = 0;
+        int high = items.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) >>> 1;
+            if (items[mid] == target) {
+                // 左边界向右收缩
+                low = mid + 1;
+            } else if (items[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        // 循环的结束条件为 low == high -1
+        if (high < 0 || items[high] != target) {
+            return -1;
+        }
+
+        return high;
+    }
 }
